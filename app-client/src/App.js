be7a54d09b1eebce58ae47e4ab6b5064aff54d35
EdwinIvanS <sedwin118@hotmail.com>
2022-09-24 14:45:37 -0500

    agregando Skills

--- app-client/src/App.js
+++ app-client/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import NavBarC from './components/app/NavBarC';
 import Banner from './components/app/Banner';
+import Skills from './components/app/Skills';
 
 function App() {
   return (
     <div className="App">
       <NavBarC/>
       <Banner/>
+      <Skills/>
     </div>
   );
 }
