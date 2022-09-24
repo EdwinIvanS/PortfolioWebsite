fa3cdcc40ff46413c4d4cbefd5b9d028c4e56b63
EdwinIvanS <sedwin118@hotmail.com>
2022-09-24 14:00:38 -0500

    agregando Banner

--- app-client/src/App.js
+++ app-client/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import NavBarC from './components/app/NavBarC';
+import Banner from './components/app/Banner';
 
 function App() {
   return (
     <div className="App">
       <NavBarC/>
+      <Banner/>
     </div>
   );
 }
